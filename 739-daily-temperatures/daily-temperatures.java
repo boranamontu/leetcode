1147c916d127358ea57335d0d6ae3b1eabeb8a87
montu borana <montuborana18@gmail.com>
2025-12-26 18:38:20 +0530

    Time: 87 ms (5.44%) | Memory: 106.6 MB (63.73%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        Stack<Integer> s=new Stack<>();
+        int ans[]=new int[temp.length];
+        for(int i=ans.length-1;i>=0;i--){
+            if(s.isEmpty()){
+                ans[i]=0;
+                s.push(i);
+            }else if(temp[i]>=temp[s.peek()]){
+                     while(!s.isEmpty()){
+                        if(temp[i]>=temp[s.peek()]){
+                            s.pop();
+                        }else{
+                            break;
+                        }
+                        
+                     }
+                     if(s.isEmpty()){
+                      ans[i]=0;
+                        s.push(i);
+                     }else{
+                        ans[i]=s.peek()-i;
+                        s.push(i);
+                     }
+            }else{
+                 ans[i]=s.peek()-i;
+                        s.push(i);
+            }
+
+        }
+        return ans;
+    }
+}
