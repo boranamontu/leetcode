3d13343ebafdc9f08925f09208ed435fd0fc5b82
montu borana <montuborana18@gmail.com>
2026-01-01 06:39:33 +0530

    Time: 12 ms (8.20%) | Memory: 46.2 MB (11.88%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<tickets.length;i++){
+            q.add(i);
+        }int stepcount=0;
+        while(tickets[k]!=0){
+            tickets[q.peek()]--;
+            if(tickets[q.peek()]==0 ){
+              q.remove();
+            }else{q.add(q.remove());
+            }
+            stepcount++;
+            
+        }
+        return stepcount;
+    }
+}
