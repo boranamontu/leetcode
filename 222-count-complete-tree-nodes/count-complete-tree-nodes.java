9bd7f738f49d2c64dbb285377de26287e7aa1305
montu borana <montuborana18@gmail.com>
2026-02-06 18:20:01 +0530

    Time: 0 ms (100.00%) | Memory: 49.5 MB (63.83%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,21 +14,12 @@
  * }
  */
 class Solution {
-    public int helper(TreeNode root,int count){
-        if(root==null){
-            return 0;
-        }
-        int left=helper(root.left,count);
-         int rig=helper(root.right,count);
-
-
-        return left+rig+1;
-    }
+   
     public int countNodes(TreeNode root) {
-        int count=0;
+      
         if(root==null){
             return 0;
         }
-        return helper(root,count);
+        return countNodes(root.left)+countNodes(root.right)+1;
     }
 }
