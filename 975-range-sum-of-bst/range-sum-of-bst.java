b7b8cc1a86be0bb2ffd9dd6fe1ad79702faa79cc
montu borana <montuborana18@gmail.com>
2026-03-11 19:49:45 +0530

    Time: 416 ms (6.22%) | Memory: 53.3 MB (99.03%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -14,18 +14,14 @@
  * }
  */
 class Solution {
-    public static int helper(List<Integer> ll,TreeNode root, int low, int high){
+    public static int helper(List<Integer> ll,TreeNode root, int low, int high,int sum){
            if(root==null)return 0;
 
-        //    if(low<root.val && high<root.val){
-        //      helper(sum,root.left,low,high);
-        //    }else if(low>root.val && high>root.val){
-        //      helper(sum,root.right,low,high);
-        //    }else{
-        //     if(root.val>=low || root.val<=high){
-        //          sum=sum+root.val;
-                
-        //     }
+           if(low<root.val && high<root.val){
+             helper(ll,root.left,low,high,sum);
+           }else if(low>root.val && high>root.val){
+             helper(ll,root.right,low,high,sum);
+           }else{
             
         //          helper(sum,root.left,low,high);
       
@@ -37,11 +33,12 @@ class Solution {
                  ll.add(root.val);
                 
             }
-                        helper(ll,root.left,low,high);
+                        helper(ll,root.left,low,high,sum);
       
-                 helper(ll,root.right,low,high);
-                       int sum=0;
-                 for(int i=0;i<ll.size();i++){
+                 helper(ll,root.right,low,high,sum);
+                     
+                
+           } for(int i=0;i<ll.size();i++){
                     sum=sum+ll.get(i);
                  }
                  return sum;
@@ -50,7 +47,8 @@ class Solution {
     }
     public int rangeSumBST(TreeNode root, int low, int high) {
       List<Integer> ll=new ArrayList<>();
-     return  helper(ll,root,low,high);
+      int sum=0;
+     return  helper(ll,root,low,high,sum);
       
     }
 }
