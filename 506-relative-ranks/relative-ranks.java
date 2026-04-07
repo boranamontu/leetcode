dc9ba906a3fc4ec71b7a2b090770f18c6f3d0d93
montu borana <montuborana18@gmail.com>
2026-04-07 15:48:24 +0530

    Time: 8 ms (94.23%) | Memory: 47.3 MB (94.07%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.java
@@ -0,0 +1,35 @@
+class Solution {
+     static public  class sort implements Comparable<sort>{
+            int val;
+            int idx;
+            public  sort(int idx,int val){
+                this.val = val;
+                this.idx = idx;
+            }
+
+            @Override
+            public int compareTo(sort curr){
+                return this.val - curr.val;
+            }
+        }
+    public String[] findRelativeRanks(int[] score) {
+      PriorityQueue<sort> pq = new PriorityQueue<>();
+      String str[] = new String[score.length];
+      for(int i=0;i<score.length;i++){
+        pq.add(new sort(i,score[i]));
+      }
+      while(!pq.isEmpty()){
+        if(pq.size()>3){
+            str[pq.remove().idx] = String.valueOf(pq.size()+1);
+        }else if(pq.size() == 3 ){
+            str[pq.remove().idx] = "Bronze Medal";
+        }else if(pq.size() == 2 ){
+            str[pq.remove().idx] = "Silver Medal";
+        }else if(pq.size() == 1 ){
+            str[pq.remove().idx] = "Gold Medal";
+        }
+      }
+      
+      return str;
+    }
+}
