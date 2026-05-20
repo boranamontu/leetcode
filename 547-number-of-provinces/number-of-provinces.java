314e1ffba52de91b522f2950030ab0ba4b130d71
montu borana <montuborana18@gmail.com>
2026-05-20 06:33:55 +0530

    Time: 15 ms (5.16%) | Memory: 47.9 MB (22.30%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,65 @@
+class Solution {
+     public static class edge{
+        int src;
+        int dest;
+        public edge(int src,int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+   
+   public static int NoOfProvinces(ArrayList<edge> graph[]){
+        boolean visit[] = new boolean[graph.length];
+        int count = 0;
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for (int i = 0; i < visit.length; i++) {
+              if(!visit[i]){
+                q.add(i);
+                bfs(graph, visit, q);
+                count++;
+              }
+                
+            }
+        
+        return count;
+
+    }
+    public static void bfs(ArrayList<edge>[] graph,boolean visit[],Queue<Integer> q){
+        
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(visit[curr] == false){
+                System.out.print(curr+" ");
+                visit[curr]= true;
+                for(int i = 0 ; i < graph[curr].size();i++){
+                    edge e = graph[curr].get(i);
+                    
+                    q.add(e.dest);
+                    
+                }
+            }
+        }
+
+
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int v = isConnected.length;
+        ArrayList<edge> graph[] = new ArrayList[v];
+        for(int i = 0 ; i < v ; i++){
+            graph[i] = new ArrayList<>();
+        }
+        //insert
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if( isConnected[i][j] == 1  ){
+                    graph[i].add(new edge(i, j));
+                }
+            }
+        }
+
+      return NoOfProvinces(graph);
+
+    }   
+}
