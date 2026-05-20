ef1004cd7657cdd5fadaaa41dc54cc9278d31e01
montu borana <montuborana18@gmail.com>
2026-05-20 07:49:54 +0530

    Time: 2 ms (53.64%) | Memory: 46.6 MB (12.31%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,50 @@
+class Solution {
+     public static int NoOfProvinces(List<List<Integer>> rooms){
+        boolean visit[] = new boolean[rooms.size()];
+        int count = 0;
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for (int i = 0; i < visit.length; i++) {
+              if(!visit[i]){
+                q.add(i);
+                bfs(rooms, visit, q);
+                count++;
+              }
+                
+            }
+        
+        return count;
+
+    }
+    public static void bfs(List<List<Integer>> rooms,boolean visit[],Queue<Integer> q){
+        
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(visit[curr] == false){
+                
+                visit[curr]= true;
+                for(int i = 0 ; i < rooms.get(curr).size();i++){
+                    
+                    if(visit[rooms.get(curr).get(i)] == false){
+                        q.add(rooms.get(curr).get(i));
+                    }
+                    
+                    
+                }
+            }
+        }
+
+
+    }
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+         int v = rooms.size();
+        
+       
+
+        int count = NoOfProvinces(rooms);
+        return count == 1;
+
+    }
+}
