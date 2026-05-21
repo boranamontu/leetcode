6ace91cbc90086d168017da13bdffa4d5943e78f
montu borana <montuborana18@gmail.com>
2026-05-21 06:09:21 +0530

    Time: 33 ms (5.23%) | Memory: 46.5 MB (10.94%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -2,15 +2,15 @@ class Solution {
     public boolean judgeCircle(String moves) {
          int i = 0;
          int j = 0;
-   for(int c = 0 ; c < moves.length(); c++){
+    for(int c = 0 ; c < moves.length(); c++){
         if("R".equals(String.valueOf(moves.charAt(c)))){
             j = j + 1;
-        }else if("L".equals(String.valueOf(moves.charAt(c)))){
+        } if("L".equals(String.valueOf(moves.charAt(c)))){
             
             j = j - 1;
-        }else if("U".equals(String.valueOf(moves.charAt(c)))){
+        }if("U".equals(String.valueOf(moves.charAt(c)))){
             i = i - 1;
-        }else if("D".equals(String.valueOf(moves.charAt(c))) ){
+        } if("D".equals(String.valueOf(moves.charAt(c))) ){
             i = i + 1;
         }
     }
