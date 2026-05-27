83b1d5c6642e17599dd7fe7cc9be137e6fc5b443
montu borana <montuborana18@gmail.com>
2026-05-27 06:29:50 +0530

    Time: 2 ms (9.40%) | Memory: 46 MB (95.19%) - LeetSync

---
+++ 563-binary-tree-tilt/binary-tree-tilt.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static int helper(TreeNode root,Stack<Integer> s){
+        if(root == null){
+            return 0;
+        }
+        int lef = helper(root.left,s);
+        int rig = helper(root.right,s);
+
+        int copy = root.val;
+        root.val = Math.abs(lef - rig);
+        if(!s.isEmpty()){
+            s.push(s.pop() + root.val);
+        }else{
+            s.push(Math.abs(lef - rig));
+        }
+
+        return lef + rig + copy;
+    }
+    public int findTilt(TreeNode root) {
+        Stack<Integer> s = new Stack<>();
+        helper(root,s);
+        if(s.isEmpty()){
+            return 0;
+        }
+        return s.peek();
+    }
+}
