058268adec2d65b04c4e2ae46de6e3c20c971ab3
montu borana <montuborana18@gmail.com>
2026-06-04 10:44:23 +0530

    Time: 24 ms (6.72%) | Memory: 48.1 MB (7.51%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,81 @@
+class Solution {
+   public static class edge{
+        int src;
+        int dest;
+        edge(int src,int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+    } 
+    public static boolean bipartite(ArrayList<edge> graph[]){
+        int visit[] = new int[graph.length];
+         for(int i = 0 ; i < graph.length ; i++){
+            visit[i] = -1;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        
+         for(int i = 0 ; i < visit.length ; i++){
+           if(visit[i] == -1){
+            
+             q.add(i);
+             visit[i] = 1;
+             boolean notify = bfs(graph,q,visit);
+             if(notify == false){
+                for(int j = 0 ; j < graph.length ; j++){
+                      System.out.print( visit[j]+" " );
+                }
+                return false;
+             }
+           }
+        }
+        for(int i = 0 ; i < graph.length ; i++){
+           System.out.print( visit[i]+" " );
+        }
+        return true;
+    }
+    public static boolean bfs(ArrayList<edge> graph[],Queue<Integer> q,int visit[]){
+        while(!q.isEmpty()){
+            int curr = q.remove();
+        
+            
+                for(int i = 0 ; i < graph[curr].size();i++){
+                    if(visit[graph[curr].get(i).dest] == visit[curr]){
+                        System.out.println(graph[curr].get(i).dest+ "=="+ curr);
+                        System.out.println(visit[graph[curr].get(i).dest]+ "=="+visit[curr]);
+                        return false;
+                    }
+                    if(visit[graph[curr].get(i).dest] != visit[curr]  ){
+                        //-1
+                        if(visit[graph[curr].get(i).dest] == -1){
+                            q.add(graph[curr].get(i).dest);
+                            if(visit[curr] == 0){
+                                visit[graph[curr].get(i).dest] = 1;
+                            }else{
+                                visit[graph[curr].get(i).dest] = 0;
+                            }
+                        }
+                        //?
+                        //no action 
+
+                    }
+                }
+            
+        }
+        return true;
+    }
+     public static void insert(ArrayList<edge> graph[], int[][] edges){
+    for(int i = 0; i < edges.length; i++){
+        for(int j = 0; j < edges[i].length; j++){
+            graph[i].add(new edge(i, edges[i][j]));
+        }
+    }
+}
+    public boolean isBipartite(int[][] edges) {
+        ArrayList<edge> graph[] = new ArrayList[edges.length];
+        for(int i = 0 ; i < graph.length ; i++){
+            graph[i] = new ArrayList<>();
+        }
+        insert(graph,edges);
+        return bipartite(graph);
+    }
+}
