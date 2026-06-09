858860186d0acfb87d162176fb92cdb48d16c502
montu borana <montuborana18@gmail.com>
2026-06-09 15:21:46 +0530

    Time: 3 ms (1.55%) | Memory: 44.2 MB (8.20%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+    int n = matrix[0].length;
+    int i = 0;
+    while(i < m && target > matrix[i][n-1] ){
+        System.out.println(matrix[i][0]);
+        i++;
+    }
+
+    if(i >= m){
+        return false;
+    }else{
+    for(int j = 0;j < n;j++){
+        if(matrix[i][j] == target){
+            return true;
+            
+        }
+    }
+    return false;
+    }
+    }
+}
