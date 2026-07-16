f48f840f2e75b2413686e63599d859826b958936
montu borana <montuborana18@gmail.com>
2026-07-16 19:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (19.94%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        if(nums.length == 1 && nums[0] == 1){
-            return 0;
-        }
         int max_val = nums.length;
         int num_cnt = 0;
+        int max_cnt = 0;
         for(int i = 0 ; i < nums.length ; i++){
-           
             num_cnt = num_cnt + nums[i];
         }
-        int max_cnt = 0;
         while(max_val != 0){
             max_cnt += max_val;
             max_val--;
