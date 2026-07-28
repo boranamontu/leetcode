dd12eeca7a396cb7853b41bb0024df5f3f56c1ad
montu borana <montuborana18@gmail.com>
2026-07-28 18:21:41 +0530

    Time: 5 ms (1.66%) | Memory: 43.6 MB (12.38%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        for(int i = 1 ; i < word1.length ; i++){
+            word1[0] += word1[i];
+        }
+        for(int i = 1 ; i < word2.length ; i++){
+           word2[0] += word2[i];
+        }
+        System.out.println(word1[0] + " "+ word2[0]);
+        return word2[0].equals(word1[0]);
+    }
+}
