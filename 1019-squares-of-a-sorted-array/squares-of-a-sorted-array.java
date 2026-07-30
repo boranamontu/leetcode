7527d2ed0fec45b7f7c5ee56ef6531512480fd14
montu borana <montuborana18@gmail.com>
2026-07-30 19:22:27 +0530

    Time: 25 ms (5.02%) | Memory: 48 MB (16.66%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            pq.add(nums[i]*nums[i]);
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            nums[i] = pq.remove();
+        }
+        return nums;
+
+    }
+}
